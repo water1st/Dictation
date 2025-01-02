4c21c5cca6e3002ffa1372b671693ce5d852656c
zigit <water_1st@163.com>
2025-01-02 19:00:39 +0800

    修改命名空间

--- Dictation.Core/JapaneseEdgeTTSPlayer.cs
+++ Dictation.Core/JapaneseEdgeTTSPlayer.cs
@@ -1,22 +1,27 @@
 ﻿using Edge_tts_sharp;
+using Edge_tts_sharp.Model;
 using System.Linq;
 
 namespace Dictation.Core
 {
     internal class JapaneseEdgeTTSPlayer : ITTSPlayer
     {
-        public void Play(string word)
+        private readonly eVoice voice;
+        public JapaneseEdgeTTSPlayer()
         {
             var voices = Edge_tts.GetVoice();
-            var voice = voices.FirstOrDefault(c => c.ShortName == "ja-JP-NanamiNeural");
+            voice = voices.FirstOrDefault(c => c.ShortName == "ja-JP-NanamiNeural");
+        }
 
+        public void Play(string word)
+        {
             var speakWord = word;
             if (word.Contains(','))
             {
                 speakWord = word.Split(',')[0].Trim();
             }
 
-            Edge_tts.PlayText(new Edge_tts_sharp.Model.PlayOption
+            Edge_tts.PlayText(new PlayOption
             {
                 Rate = 0,
                 Volume = 1,
